f3bc4342024bcd608ccadd4992e32424f21f37d9
Min Thet Naing <bobo.b90@gmail.com>
2022-11-24 19:14:45 +0630

    Add the logic to write the downloaded content under a folder with the url domain name

--- fetch.rb
+++ fetch.rb
@@ -2,20 +2,31 @@
 
 require "mechanize"
 require "open-uri"
+require "fileutils"
 
 agent = Mechanize.new
 
+# Begin: Constant variables
+FETCHED_SITES_DIR = "fetched_sites"
+# End: Constant variables
+
 # Begin: Helper methods
-def format_file_name(url)
+def convert_to_folder_name(url)
   uri = URI.parse(url)
   uri.host
+end
+
+def create_destinated_folder(folder_name)
+  FileUtils.mkdir_p File.join(FETCHED_SITES_DIR, folder_name)
 end
 # End: Helper methods
 
 ARGV.each do |url|
   begin 
     page = agent.get(url)
-    File.open(format_file_name(url).concat(".html"), "w") do |file|
+    destinated_folder = create_destinated_folder(convert_to_folder_name(url))
+
+    File.open(File.join(destinated_folder, "index.html"), "w") do |file|
       file.write(page.content)
     end
   rescue Exception => e
