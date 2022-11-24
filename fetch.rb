cf45769c256a34d306f334de5cd430e5a788347e
Min Thet Naing <bobo.b90@gmail.com>
2022-11-24 22:41:56 +0630

    Add saving images to local folder

--- fetch.rb
+++ fetch.rb
@@ -9,15 +9,22 @@ require "open-uri"
 
 # Begin: Constant variables
 FETCHED_SITES_DIR = "fetched_sites"
+IMAGES_DIR = "images"
 METADATA_FILE = File.join(FETCHED_SITES_DIR, "metadata.json")
 # End: Constant variables
 
 # Begin: Helper methods
+def format_asset_name(asset_name)
+  # Kinda a Hack since asset value like `name.png?query=value..` does not seem to be linked properly 
+  # within downloaded local html content.
+  asset_name.split("?")[0]
+end
+
 def convert_to_folder_name(url)
   uri = URI.parse(url)
 
   # To ensure we could download different path of the same domain in different folders
-  "#{uri.host.gsub(/^www\./, '')}#{uri.path}#{uri.query}"
+  "#{uri.host.gsub(/^www\./, "")}#{uri.path}#{uri.query}"
 end
 
 def create_destinated_folder(folder_name)
@@ -52,9 +59,45 @@ def retrieve_existing_metadata
   JSON.parse(File.read(METADATA_FILE))
 end
 
+def save_and_localize_assets(folder_name, content)
+  puts "Saving assets to local folder."
+  save_and_localize_images(folder_name, content.xpath("//img[@src]"))
+end
+
+def save_and_localize_images(folder_name, images)
+  image_dir = create_destinated_folder(File.join(folder_name, IMAGES_DIR))
+
+  images.each do |image|
+    # Not able to handle base64 images
+    next if image["src"].empty? || image["src"].include?("data:image")
+
+    formatted_image_name = format_asset_name(File.basename(image["src"]))
+    puts formatted_image_name
+    File.open(File.join(image_dir, formatted_image_name), "w") do |file|
+      image_url = if URI.parse(image["src"]).host
+                    image["src"]
+                  else
+                    image["src"].prepend(folder_name).prepend("http://www.")
+                  end
+      begin
+        sleep(rand/100) # Multiple images then we would constantly be sending many requests to download assets
+        file.write(URI.open(image_url).read)
+
+        # Modify the local html content
+        image["src"] = "./#{IMAGES_DIR}/#{formatted_image_name}"
+      rescue Exception => e
+        puts "Warning: #{e.message}"
+        next
+      end
+    end
+  end
+end
+
 def save_html_content(folder_name, content)
-  puts "Saving html content to local folder `#{folder_name[0]}`"
-  File.open(File.join(folder_name, "index.html"), "w") do |file|
+  puts "Saving html content to local folder"
+
+  content_dir = create_destinated_folder(folder_name)
+  File.open(File.join(content_dir, "index.html"), "w") do |file|
     file.write(content)
   end
 end
@@ -67,8 +110,8 @@ def save_metadata(key, content)
     "images": content.xpath("//img[@src]").length,
     "last_fetch": Time.now.utc
   }
-  File.open(METADATA_FILE, "w") do |f|
-    f.write(JSON.pretty_generate(existing_metadata))
+  File.open(METADATA_FILE, "w") do |file|
+    file.write(JSON.pretty_generate(existing_metadata))
   end
 end
 # End: Helper methods
@@ -78,6 +121,7 @@ end
 @agent.log = Logger.new "fetch.log"
 urls = ARGV.reject { |arg| arg =~ /--[a-z]/}
 metadata_arg = ARGV.include?("--metadata")
+save_assets_arg = ARGV.include?("--save-assets")
 
 urls.each_with_index do |url, index|
   begin 
@@ -85,9 +129,9 @@ urls.each_with_index do |url, index|
 
     content = fetch_content(url)
     folder_name = convert_to_folder_name(url)
-    destinated_folder = create_destinated_folder(folder_name)
 
-    save_html_content(destinated_folder, content)
+    save_and_localize_assets(folder_name, content) if save_assets_arg
+    save_html_content(folder_name, content)
     if metadata_arg
       save_metadata(folder_name, content)
       print_metadata(folder_name)
