7037acc6b9396c3c0fea680c3be9e1ecaf316a31
Min Thet Naing <bobo.b90@gmail.com>
2022-11-24 19:00:52 +0630

    Add rescuing for any failure while fetching urls

--- fetch.rb
+++ fetch.rb
@@ -13,8 +13,13 @@ end
 # End: Helper methods
 
 ARGV.each do |url|
-  page = agent.get(url)
-  File.open(format_file_name(url).concat(".html"), "w") do |file|
-    file.write(page.content)
+  begin 
+    page = agent.get(url)
+    File.open(format_file_name(url).concat(".html"), "w") do |file|
+      file.write(page.content)
+    end
+  rescue Exception => e
+    puts e.message
+    next
   end
 end
